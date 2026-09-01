25eba6b674cf54576cb5df28d59de9cfc7d36b30
phoshp <emre.yavuz.842@gmail.com>
2026-09-01 15:17:50 +0300

    v0.0.4

--- BULK.ps1
+++ BULK.ps1
@@ -224,7 +224,7 @@ function Invoke-AllActions {
 
 function Show-Menu {
     Clear-Host
-    Write-Host "KDPÜ BİDB Windows Bulk Aracı v0.0.3" -ForegroundColor Yellow
+    Write-Host "KDPÜ BİDB Windows Bulk Aracı v0.0.4" -ForegroundColor Yellow
     Write-Host "--------------------------------"
     Write-Host "1.) Hepsini uygula"
     Write-Host "2.) Programları kur"
